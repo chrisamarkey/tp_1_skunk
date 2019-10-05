5188cf7d1e46d87f0d43f2e93a4a104ec50eb810
gmschaefer <gmschaefer@stthomas.edu>
2019-10-04 20:28:48 -0500

    The beginnings of test for Dice.java

--- SkunkProject/test/TestDice.java
+++ SkunkProject/test/TestDice.java
@@ -9,11 +9,14 @@ public class TestDice
 
 	@Test
 	public void testDice() {
-	  Dice testDie1 = new Dice();
-	  Dice testDie2 = new Dice();
+	  Die testDie1 = new Die();
+	  Die testDie2 = new Die();
       testDie1.roll();
       testDie2.roll();
+      int valueOne = testDie2.getLastRoll();
+      int valueTwo = testDie2.getLastRoll();
+      int total = valueTwo + valueOne;
+      StdOut.println(total);
 	}
-
-
+	
 }
