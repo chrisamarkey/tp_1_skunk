1bd94fbef1e22f127602fee58f25d0e7764cc113
gmschaefer <gmschaefer@stthomas.edu>
2019-10-04 21:12:21 -0500

    Latest for TestDice

--- SkunkProject/test/TestDice.java
+++ SkunkProject/test/TestDice.java
@@ -9,11 +9,12 @@ public class TestDice
 
 	@Test
 	public void testDice() {
-	  Die testDie1 = new Die();
-	  Die testDie2 = new Die();
+	  int[] testValues = new int[] { 1,2,3,4,5,6 };
+	  Die testDie1 = new Die(testValues);
+	  Die testDie2 = new Die(testValues);
       testDie1.roll();
       testDie2.roll();
-      int valueOne = testDie2.getLastRoll();
+      int valueOne = testDie1.getLastRoll();
       int valueTwo = testDie2.getLastRoll();
       int total = valueTwo + valueOne;
       StdOut.println(total);
