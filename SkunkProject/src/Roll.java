fb1f8591f0c26c3fe66559f66e153f970c31b5d8
chrisamarkey <chris.a.markey@seagate.com>
2019-12-16 18:04:20 -0600

    Update Roll.java
    Cleaned up code.

--- SkunkProject/src/Roll.java
+++ SkunkProject/src/Roll.java
@@ -1,6 +1,6 @@
 //import edu.princeton.cs.introcs.StdOut;
 
-//import edu.princeton.cs.introcs.StdOut;
+import edu.princeton.cs.introcs.StdOut;
 
 public class Roll {
 
@@ -8,13 +8,15 @@ public class Roll {
 	private int turnScore = 0;
 	private int gameScore = 0;
 	private Dice newDice;
+	private int playerNum = 0;
 	private boolean loseTurn = false;
-	private int playerChipCount;
+	private int playerChipCount = 50;
 
-	public int throwDice() {
+	public int throwDice(int playerNum) {
 
 		this.newDice = new Dice();
 		newDice.roll();
+		this.playerNum = playerNum;
 
 		// Double Skunk rolled
 		if (newDice.getLastRoll() == 2) {
@@ -22,16 +24,9 @@ public class Roll {
 			turnScore = 0;
 			gameScore = 0;
 			this.playerChipCount += -4;
+//			newGame.setPlayerGameScore(playerNum) = 0; //12-14-19
+			StdOut.println("***You rolled a double skunk!!!");
 			loseTurn = true;
-		}
-
-		// Skunk rolled
-		else if ((newDice.die1GetLastRoll() == 1 || newDice.die2GetLastRoll() == 1)) {
-			rollScore = 0;
-			turnScore = 0;
-			this.playerChipCount += -1;
-			loseTurn = true;
-
 		}
 
 		// Skunk Deuce rolled
@@ -39,6 +34,16 @@ public class Roll {
 			rollScore = 0;
 			turnScore = 0;
 			playerChipCount += -2;
+			StdOut.println("**You rolled a skunk deuce!!");
+			loseTurn = true;
+		}
+		
+		// Skunk rolled
+		else if ((newDice.die1GetLastRoll() == 1 || newDice.die2GetLastRoll() == 1)) {
+			rollScore = 0;
+			turnScore = 0;
+			this.playerChipCount += -1;
+			StdOut.println("*You rolled a skunk!");
 			loseTurn = true;
 
 		} else
@@ -58,6 +63,14 @@ public class Roll {
 		return this.rollScore;
 	}
 
+	public int getDie1Score() {
+		return this.newDice.die1GetLastRoll();
+	}
+	
+	public int getDie2Score() {
+		return this.newDice.die2GetLastRoll();
+	}
+	
 	public int updateChipCount() {
 		return this.playerChipCount;
 	}
@@ -70,6 +83,10 @@ public class Roll {
 		return this.gameScore;
 	}
 
+	public int getPlayerChipCounts() {
+		return this.playerChipCount;
+	}
+	
 	public int updateKitty() {
 		return -this.playerChipCount;
 	}
